bf46679c393327195d4b6444fe09174d96230cfa
unknown <nabarazz01@outlook.com>
2024-08-26 10:59:31 +0545

    website

--- models/website_highlight.py
+++ models/website_highlight.py
@@ -1,12 +1,10 @@
-# models/website_highlight.py
-
 from odoo import models, fields
 
 
 class WebsiteHighlight(models.Model):
     _name = "website.highlight"
-    _description = "Website Highlights"
+    _description = "Website Highlight"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string="Name", required=True)
     message = fields.Text(string="Message", required=True)
     active = fields.Boolean(string="Active", default=True)
